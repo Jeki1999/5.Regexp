6d3a77998037ad3b1ad2d66023c3fce5092c3010
Evgeniy Veselskiy <jeka.vesel@mail.ru>
2022-09-20 18:34:36 +0600

    finish_2

--- main.py
+++ main.py
@@ -3,43 +3,46 @@ import csv
 import re
 
 
-with open("phonebook_raw.csv") as f:
-  rows = csv.reader(f, delimiter=",")
-  contacts_list = list(rows)
-pprint(contacts_list)
+PHONE_PATTERN = r'(\+7|8)*[\s\(]*(\d{3})[\)\s-]*(\d{3})[-]*(\d{2})[-]*(\d{2})[\s\(]*(доб\.)*[\s]*(\d+)*[\)]*'
+PHONE_SUB = r'+7(\2)-\3-\4-\5 \6\7'
 
-new_contacts_list = []
-pattern_number = "(\+7|8)*[\s\(]*(\d{3})[\))\s-]*(\d{3})[-]*(\d{2})[-]*(\d{2})(\s)*(\()*(доб.)*[\s]*(\d+)*[\)]*"
-new_number = r'+7(\2)\3-\4-\5\6\8\9'
-def main(contacts_list):
-    for contact in contacts_list:
-        name = ' '.join(contact[:3]).split(' ')
-        new_list = [name[0], name[1], name[2], contact[3], contact[4], re.sub(pattern_number, new_number, contact[5]), contact[6]]
-        new_contacts_list.append(new_list)
-    return merge(new_contacts_list)
+with open("phonebook_raw.csv", encoding="utf-8") as f:
+    rows = csv.reader(f, delimiter=",")
+    contacts_list = list(rows)
 
 
-def merge(contacts):
+
+def main(contact_list: list):
+    new_list = list()
+    for item in contact_list:
+        full_name = ' '.join(item[:3]).split(' ')
+        result = [full_name[0], full_name[1], full_name[2], item[3], item[4],
+                  re.sub(PHONE_PATTERN, PHONE_SUB, item[5]),
+                  item[6]]
+        new_list.append(result)
+    return union(new_list)
+
+
+def union(contacts: list):
     for contact in contacts:
+        first_name = contact[0]
+        last_name = contact[1]
         for new_contact in contacts:
-            if contact[0] == new_contact[0] and contact[1] == new_contact[1]:
-                if contact[2] == "":
-                    contact[2] = new_contact[2]
-                if contact[3] == "":
-                    contact[3] = new_contact[3]
-                if contact[4] == "":
-                    contact[4] = new_contact[4]
-                if contact[5] == "":
-                    contact[5] = new_contact[5]
-                if contact[6] == "":
-                    contact[6] = new_contact[6]
-    new_list = []
+            new_first_name = new_contact[0]
+            new_last_name = new_contact[1]
+            if first_name == new_first_name and last_name == new_last_name:
+                if contact[2] == "": contact[2] = new_contact[2]
+                if contact[3] == "": contact[3] = new_contact[3]
+                if contact[4] == "": contact[4] = new_contact[4]
+                if contact[5] == "": contact[5] = new_contact[5]
+                if contact[6] == "": contact[6] = new_contact[6]
+    result_list = list()
     for i in contacts:
-        if i not in new_list:
-            new_list.append(i)
-    return new_list
+        if i not in result_list:
+            result_list.append(i)
 
-with open("phonebook.csv", "w") as f:
-  datawriter = csv.writer(f, delimiter=',')
-  # Вместо contacts_list подставьте свой список
-  datawriter.writerows(contacts_list)
+    return result_list
+
+with open("phonebook.csv", "w", encoding="utf-8") as f:
+    datawriter = csv.writer(f, delimiter=',')
+    datawriter.writerows(main(contacts_list))
